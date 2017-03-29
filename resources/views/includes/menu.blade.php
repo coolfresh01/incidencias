60d8557313a6f816fca3df8d57708433830e641e
coolfresh01 <coolfresh83@gmail.com>
2017-03-29 09:10:29 -0500

    Linea base

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,19 @@
+<div class="panel panel-primary">
+	<div class="panel-heading">Menu</div>
+
+	<div class="panel-body">
+		<ul class="nav nav-pills nav-stacked">
+			@if (auth()->check())
+				<li><a href="">Dashboard</a></li>
+				<li><a href="">Ver Incidencias</a></li>
+				<li><a href="">Reportar Incidencia</a></li>
+				<li><a href="">Administracion</a></li>
+			@else
+				<li><a href="">Bienvenido</a></li>
+				<li><a href="">Instrucciones</a></li>
+				<li><a href="">Creditos</a></li>
+			@endif
+		</ul>
+	</div>
+</div>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="panel panel-primary">
+    <div class="panel-heading">Bienvenido - Sistema de Gestion de Incidencias</div>
+    <div class="panel-body">
+        <P>Hola</P>
+    </div>
+</div>
+@endsection
